786c6fd18fe9a573c9aff48a5ccd3bc680ae14d9
shijinke1990 <shijinke1990@qq.com>
2024-06-30 00:01:45 +0800

    feat: Add 'getAppName' endpoint to retrieve the application name
    This commit adds a new endpoint 'getAppName' to the 'AppController' class in order to retrieve the application name. The application name is injected as a dependency using the 'APP_NAME' token and is returned as a comma-separated string.
    
    Code changes:
    - Added 'getAppName' method to 'AppController' class
    - Injected 'APP_NAME' token in the constructor of 'AppController'
    - Returned the application name as a comma-separated string in the 'getAppName' method
    
    Recent user commit:
    - chore: Add initial project files and configurations

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
+import { Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('APP_NAME') private readonly appName: string[],
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('appName')
+  getAppName(): string {
+    return this.appName.join(', ');
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,12 @@ import { AppService } from './app.service';
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'APP_NAME',
+      useValue: ['NestJS', 'Express', 'TypeScript', 'Node.js', 'JavaScript'],
+    },
+  ],
 })
 export class AppModule {}
